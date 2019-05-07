c00d2ee957f48e6b28e673518db9657ef773cd1c
Alex Mireles <anchises1968@gmail.com>
2019-05-07 10:45:20 -0700

    restored murals page to original

--- src/cms/preview-templates/MuralsPagePreview.js
+++ src/cms/preview-templates/MuralsPagePreview.js
@@ -3,9 +3,14 @@ import PropTypes from 'prop-types'
 import { MuralsPageTemplate } from '../../templates/murals-page'
 
 const MuralsPagePreview = ({ entry, getAsset }) => {
-  
+  const entryBlurbs = entry.getIn(['data', 'intro', 'blurbs'])
+  const blurbs = entryBlurbs ? entryBlurbs.toJS() : []
 
+  const entryTestimonials = entry.getIn(['data', 'testimonials'])
+  const testimonials = entryTestimonials ? entryTestimonials.toJS() : []
 
+  const entryPricingPlans = entry.getIn(['data', 'pricing', 'plans'])
+  const pricingPlans = entryPricingPlans ? entryPricingPlans.toJS() : []
 
   return (
     <MuralsPageTemplate
@@ -13,8 +18,30 @@ const MuralsPagePreview = ({ entry, getAsset }) => {
       title={entry.getIn(['data', 'title'])}
       heading={entry.getIn(['data', 'heading'])}
       description={entry.getIn(['data', 'description'])}
-
-
+      intro={{ blurbs }}
+      main={{
+        heading: entry.getIn(['data', 'main', 'heading']),
+        description: entry.getIn(['data', 'main', 'description']),
+        image1: {
+          image: getAsset(entry.getIn(['data', 'main', 'image1', 'image'])),
+          alt: entry.getIn(['data', 'main', 'image1', 'alt']),
+        },
+        image2: {
+          image: getAsset(entry.getIn(['data', 'main', 'image2', 'image'])),
+          alt: entry.getIn(['data', 'main', 'image2', 'alt']),
+        },
+        image3: {
+          image: getAsset(entry.getIn(['data', 'main', 'image3', 'image'])),
+          alt: entry.getIn(['data', 'main', 'image3', 'alt']),
+        },
+      }}
+      fullImage={entry.getIn(['data', 'full_image'])}
+      testimonials={testimonials}
+      pricing={{
+        heading: entry.getIn(['data', 'pricing', 'heading']),
+        description: entry.getIn(['data', 'pricing', 'description']),
+        plans: pricingPlans,
+      }}
     />
   )
 }
