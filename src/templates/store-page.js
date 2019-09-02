97b058b6924f69fc084e969cdfe38b9270526b4d
Alex Mireles <anchises1968@gmail.com>
2019-09-02 15:22:30 +0200

    defined layout in store-page

--- src/templates/store-page.js
+++ src/templates/store-page.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
+import Layout from '../components/Layout'
 
 
 import styled from 'styled-components'
